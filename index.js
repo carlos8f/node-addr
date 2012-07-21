b84c3e8ed3f845709e5f24358aa3b26246175f4d
Carlos Rodriguez <carlos@s8f.org>
2012-07-21 14:22:34 -0700

    Initial code

---
+++ index.js
@@ -0,0 +1,22 @@
+
+module.exports = function addr(req, proxies) {
+  if (!req.headers['x-forwarded-for']) {
+    return req.connection.remoteAddress;
+  }
+  if (typeof proxies === 'string') {
+    proxies = [proxies];
+  }
+  if (proxies && proxies.indexOf(req.connection.remoteAddress) === -1) {
+    return false;
+  }
+  var addrs = req.headers['x-forwarded-for'].split(/\s?,\s?/);
+  var ip = addrs.shift(), proxy;
+  if (proxies) {
+    while (proxy = address.pop()) {
+      if (proxies.indexOf(proxy) === -1) {
+        return false;
+      }
+    }
+  }
+  return ip;
+};
---
+++ test/basic.js
@@ -0,0 +1,39 @@
+var addr = require('../')
+  , assert = require('assert')
+  , http = require('http')
+  , request = require('request')
+  , port = 55221
+  , baseUrl = 'http://localhost:' + port
+  ;
+
+describe('basic test', function() {
+  before(function(done) {
+    http.createServer(function(req, res) {
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      var proxies = (req.url === '/not-trusted') ? '1.2.3.4' : null;
+      res.write(JSON.stringify({addr: addr(req, proxies)}));
+      res.end();
+    }).listen(port, done);
+  });
+  it('returns a valid ip', function(done) {
+    request({url: baseUrl + '/', json: true}, function(err, res, data) {
+      assert.equal(res.statusCode, 200);
+      assert(/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.exec(data.addr));
+      done();
+    });
+  });
+  it('returns a proxy ip', function(done) {
+    request({url: baseUrl + '/', headers: {'X-Forwarded-For': '55.55.55.55'}, json: true}, function(err, res, data) {
+      assert.equal(res.statusCode, 200);
+      assert.strictEqual(data.addr, '55.55.55.55');
+      done();
+    });
+  });
+  it('returns false if proxy isn\'t trusted', function(done) {
+    request({url: baseUrl + '/not-trusted', headers: {'X-Forwarded-For': '55.55.55.55'}, json: true}, function(err, res, data) {
+      assert.equal(res.statusCode, 200);
+      assert.strictEqual(data.addr, false);
+      done();
+    });
+  });
+});
