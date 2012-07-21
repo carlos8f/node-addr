a2d218392071f8b65fa1f56817cfeb1c13a4756f
Carlos Rodriguez <carlos@s8f.org>
2012-07-21 14:30:09 -0700

    Add examples

---
+++ examples/simple.js
@@ -0,0 +1,13 @@
+var addr = require('../')
+  , http = require('http')
+  , port = 3000
+  , proxies = ['127.0.0.1', '1.2.3.4']
+  ;
+
+http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.write(JSON.stringify({addr: addr(req, proxies)}));
+  res.end();
+}).listen(port, function() {
+  console.log('test server running on port ' + port);
+});
