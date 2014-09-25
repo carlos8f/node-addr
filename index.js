9f8e9a3da1ed2082a8277030e2e6646be6fab4b6
Carlos Rodriguez <carlos@s8f.org>
2014-09-25 14:22:16 -0700

    use peername if present

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 module.exports = function addr(req, proxies) {
   if (!req.headers['x-forwarded-for']) {
-    return req.connection.remoteAddress;
+    return req.connection._peername && req.connection._peername.address || req.connection.remoteAddress;
   }
   if (typeof proxies === 'string') {
     proxies = [proxies];
@@ -9,7 +9,7 @@ module.exports = function addr(req, proxies) {
   if (proxies && proxies.indexOf(req.connection.remoteAddress) === -1) {
     // Don't honor the x-forwarded-for header if request is not from a trusted
     // source.
-    return req.connection.remoteAddress;
+    return req.connection._peername && req.connection._peername.address || req.connection.remoteAddress;
   }
   return req.headers['x-forwarded-for'].split(/\s?,\s?/).shift();
 };
